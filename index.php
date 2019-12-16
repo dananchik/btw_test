46fda4085aec5c101a2453ea0077fa95d845978a
dananchik <kuraveshka@gmail.com>
2019-12-16 14:42:50 +0200

    Базовая структура проекта

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//require_once 'brain/Router.php';
+use project_app\brain\Router;
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\','/',$class);
+    require_once $path . '.php';
+});
+
+$router = new Router;
+
+?>
---
+++ project_app/brain/Router.php
@@ -0,0 +1,10 @@
+<?php
+namespace project_app\brain;
+
+class Router
+{
+    function __construct()
+    {
+        echo 'я роутер';
+    }
+}
